4f485c7eeecc3e8ab00d2030fdaf6e85b1e870e3
Hozifa Wasfy <hozwasfy@gmail.com>
2023-11-29 18:59:58 +0100

    utils file

--- utils.py
+++ utils.py
@@ -65,15 +65,7 @@ def load_checkpoint(checkpoint_file, model, optimizer, lr):
     for param_group in optimizer.param_groups:
         param_group["lr"] = lr
 
-def seed_everything(seed=42):
-    os.environ['PYTHONHASHSEED'] = str(seed)
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
+
 
 def generate_examples(gen, steps, truncation=0.7, n=100):
     """
